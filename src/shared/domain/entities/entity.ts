7d9b18af1bb605eb5790bb0f7983100efbe9f70e
Cristiano Mendes <cristianovelkan@gmail.com>
2025-01-29 08:46:13 -0300

    Abstract class entity + tests

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from 'node:crypto'
+
+export abstract class Entity<Props = any> {
+  public readonly _id: string
+  public readonly props: Props
+  constructor(props: Props, id?: string) {
+    this.props = props
+    this._id = id || randomUUID()
+  }
+  get id() {
+    return this._id
+  }
+  toJSON(): Required<{ id: string } & Props> {
+    return {
+      id: this._id,
+      ...this.props,
+    } as Required<{ id: string } & Props>
+  }
+}
