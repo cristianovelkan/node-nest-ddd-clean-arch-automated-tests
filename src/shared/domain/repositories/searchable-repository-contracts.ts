0cb1198b8f1798b1e178ceb28bb5fca46a8c1f76
Cristiano Mendes <cristianovelkan@gmail.com>
2025-02-18 08:41:06 -0300

    Added SearchableRepositoryInterface

---
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -0,0 +1,9 @@
+import { Entity } from '../entities/entity'
+import { RepositoryInterface } from './repository-contracts'
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  SearchInput,
+  SearchOutput,
+> extends RepositoryInterface<E> {
+  search(props: SearchInput): Promise<SearchOutput>
+}
